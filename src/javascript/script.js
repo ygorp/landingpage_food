0daa63df635394448ed65086abf5042023b44890
ygorp <ygor-nogueira@hotmail.com>
2024-11-13 17:56:44 -0300

    finalizado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -36,5 +36,23 @@ $(document).ready(function () {
         origin: 'left',
         duration: 2000,
         distance: '20%'
+    });
+
+    ScrollReveal().reveal('.dish', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('#testimonial_chef', {
+        origin: 'left',
+        duration: 1000,
+        distance: '20%'
+    });
+
+    ScrollReveal().reveal('.feedback', {
+        origin: 'right',
+        duration: 1000,
+        distance: '20%'
     })
 });
