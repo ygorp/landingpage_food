f8e24a9b0726fc2aab515b55aa09c2e29baf8612
ygorp <ygor-nogueira@hotmail.com>
2024-11-13 17:30:10 -0300

    fazendo script para animações

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,4 +3,38 @@ $(document).ready(function () {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav_item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        let activeSection = 0;
+        const scroll = $(window).scrollTop() - header.outerHeight();
+
+        if (scroll <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - header.outerHeight();
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scroll >= sectionTop && scroll < sectionBottom) {
+                activeSection = i;
+                return false;
+            }
+        })
+
+        $(navItems[activeSection]).addClass('active').siblings().removeClass('active');
+    });
+
+    ScrollReveal().reveal('#cta', {
+        origin: 'left',
+        duration: 2000,
+        distance: '20%'
+    })
 });
